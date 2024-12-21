4a0a58075c3b3d8e921ccfacb365dd249179e63f
unknown <rajatraj48@gmail.com>
2024-12-21 17:11:00 +0530

    changes befor lunch in featuredPost

--- blog/src/layout/MainLayout.jsx
+++ blog/src/layout/MainLayout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
-    <div className="px-4 md:px-8 lg:px-16 lx:px-32 2xl:px-64">
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-32">
       <Navbar />
       <Outlet />
     </div>
